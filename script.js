adc0c09cfa71c0498dace4d2d7fd1afafca3e64c
Hamid <hcassama@uoguelph.ca>
2023-12-16 21:06:57 -0500

    Persistency is partially working. Folders are saved along with names but each task is not saved

--- script.js
+++ script.js
@@ -11,8 +11,22 @@ const folderSection = document.querySelector(".folderSection");
 const folderTextField = document.querySelector(".folderName");
 
 var numFolders = 0;
+var allFolders = [];
+var allFolderNames = [];
 
+/*
 
+const getFolders = () => {
+    let folders;
+    if(localStorage.getItem !== null) {
+        folders = JSON.parse(localStorage.getItem('folders'));
+        return folders;
+    } else {
+        folders = [];
+    }
+}
+
+*/
 
 
 
@@ -63,6 +77,7 @@ function configFolder() {
 function addFolder() {
 
     numFolders++;
+  
     
     configFolderPopup.style.visibility = "hidden"; //hide the folder name pop up menu
 
@@ -115,6 +130,19 @@ function addFolder() {
     newFolder.appendChild(addTaskButton);
     folderSection.append(newFolder);
     
+   // localStorage.setItem("numFolders", numFolders);
+
+    
+    allFolders.push(newFolder);
+    allFolderNames.push(name.textContent);
+
+    jsonNumFolders = JSON.stringify(numFolders);
+    jsonAllFolders = JSON.stringify(allFolders);
+    jsonAllFolderNames = JSON.stringify(allFolderNames);
+
+    localStorage.setItem("numFolders", jsonNumFolders);
+    localStorage.setItem("allFolders", jsonAllFolders);
+    localStorage.setItem("allFolderNames", jsonAllFolderNames);
     
     folderTextField.value = "";
 }
@@ -125,7 +153,99 @@ function cancelFolder() {
 
 setGreetingMessage();
 setInterval(setGreetingMessage, 1000);
+loadData();
 
 
 
+function showLoadedFolders() {
 
+    let num = localStorage.getItem("numFolders");
+    let folderNames = JSON.parse(localStorage.getItem("allFolderNames"));
+
+    for (let i=0; i<num; i++) {
+
+        numFolders++;
+        let newFolder = document.createElement("div");  //create the new folder
+        newFolder.classList.add("newFolder");
+
+        let name = document.createElement("h4");  //add name of folder
+        name.classList.add("folderNames")
+        console.log(JSON.stringify(folderNames[i]))
+        name.textContent = folderNames[i]; //gets the correct name based off position in array
+
+        let addTaskButton = document.createElement("button");
+        addTaskButton.classList.add("addTaskButton");
+        addTaskButton.textContent = "+";
+    
+        addTaskButton.addEventListener("click", function() {
+            let newTask = document.createElement("input");
+            newTask.classList.add("task");
+            newTask.placeholder = "Type task here";
+    
+            let checkBox = document.createElement("input");
+            checkBox.type = "checkbox";
+            checkBox.classList.add("checkBox");
+            newFolder.appendChild(checkBox);
+            newFolder.appendChild(newTask);
+    
+            checkBox.addEventListener("change", function() {
+                newTask.remove();
+                checkBox.remove();
+            });
+    
+            document.onkeydown = (event) => {
+                if(event.key == "Enter"){
+        
+                    if(newTask.value !== ""){
+                        console.log();
+                    
+                        newTask.blur();
+                        
+                    }
+                }
+            }
+            
+        });
+
+        newFolder.appendChild(name);
+        newFolder.appendChild(addTaskButton);
+        folderSection.append(newFolder);
+
+        allFolders.push(newFolder);
+        allFolderNames.push(name.textContent);
+
+        jsonNumFolders = JSON.stringify(numFolders);
+        jsonAllFolders = JSON.stringify(allFolders);
+        jsonAllFolderNames = JSON.stringify(allFolderNames);
+
+        localStorage.setItem("numFolders", jsonNumFolders);
+        localStorage.setItem("allFolders", jsonAllFolders);
+        localStorage.setItem("allFolderNames", jsonAllFolderNames);
+    }
+
+
+
+    
+
+   
+}
+
+
+function loadData () {
+    const folderData = JSON.parse(localStorage.getItem('allFolders'));
+    //localStorage.clear();
+    //console.log(folderData)
+
+    
+    if(folderData !== null) {
+        showLoadedFolders();
+        
+
+    } else {
+
+        console.log("it is null")
+
+    }
+}
+
+//localStorage.clear();
